04ea09ea185c63ab3d5607ebd608581e0857bda4
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-01-21 17:35:30 +0000

    Added decrease in temperature

--- js/interface.js
+++ js/interface.js
@@ -1,9 +1,17 @@
 var thermostat = new Thermostat();
 
 $(document).ready(function(){
+
   $('h1').text(thermostat.temperature);
+
   $('.increase-temp').on('click', function() {
     thermostat.increaseTemperature();
     $('h1').text(thermostat.temperature);
   });
+
+  $('.decrease-temp').on('click', function(){
+    thermostat.decreaseTemperature();
+    $('h1').text(thermostat.temperature);
+  });
+
 });
